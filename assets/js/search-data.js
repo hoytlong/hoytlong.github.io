c736e0199ec3a439ffa669f65f54fb53e7559a61
hoytlong <hoytlong@uchicago.edu>
2025-10-31 01:05:00 +0000

    Deploying to gh-pages from @ hoytlong/hoytlong.github.io@02920829b0465e33b082a4decffdbab332fb24c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,11 +436,6 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
@@ -451,11 +446,6 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
