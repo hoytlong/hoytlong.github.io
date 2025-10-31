7dd47a2d70a339a774a4e8f47e88959cec36f7d2
hoytlong <hoytlong@uchicago.edu>
2025-10-31 00:48:23 +0000

    Deploying to gh-pages from @ hoytlong/hoytlong.github.io@68f32baecf8ac470fc35bde4929faa855ae6747a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,9 +426,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-humanistic-ai",
+          title: 'Humanistic AI',
+          description: "Reimagining Humanistic Pursuits in the Age of Generated Media",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
