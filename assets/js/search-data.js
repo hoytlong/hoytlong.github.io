a95aae610fe983581228210a65e94639a2a4ca5a
hoytlong <hoytlong@uchicago.edu>
2025-11-01 15:42:28 +0000

    Deploying to gh-pages from @ hoytlong/hoytlong.github.io@1a93d79ddcbbe22f88197225b0f9f7e684fd31a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "",
+          description: "ongoing and past research collaborations",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
