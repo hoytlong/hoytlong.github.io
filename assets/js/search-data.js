a8d82962e6b50b85dd9af917b7ff4e81f3beaeaf
hoytlong <hoytlong@uchicago.edu>
2025-11-01 16:52:03 +0000

    Deploying to gh-pages from @ hoytlong/hoytlong.github.io@c78bb62ef358dc6c7b5f5d0e1ddccdcab469fbab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-niche-worlds",
+          title: 'Niche Worlds',
+          description: "A co-authored book project on how streaming platforms change how we think about attention and reception.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/10_project/";
             },},{id: "projects-cultural-intelligence-lab",
